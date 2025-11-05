ee978f6aadcda8b7884a8724661d40e32e7bfdc1
s2selyn <s2selyn@gmail.com>
2025-11-05 20:50:56 +0900

    file methods without comments

--- SimpleProject/src/main/java/com/kh/spring/product/model/service/ProductSaveServiceImpl.java
+++ SimpleProject/src/main/java/com/kh/spring/product/model/service/ProductSaveServiceImpl.java
@@ -44,12 +44,6 @@ public class ProductSaveServiceImpl implements ProductSaveService {
 //		}
 		
 		// 2. 값에 대한 유효성 검증
-		// 상품명, 가격, 카테고리 번호, 판매자번호(위에서 받아옴), 상품설명, 파일이 있다면 이름 변경
-		// 2_1) 상품명, 상품설명 확인
-//		if(product.getProductName().trim().isBlank() || product.getDetailContent().trim().isBlank()) {
-//			throw new InvalidArgumentsException("상품 정보를 올바르게 입력해주세요");
-//		}
-		// 책임분리함
 		validateContent(product);
 		
 		// 2_2) 가격을 올바로 입력했는지 -> 이거 해야하는지? 넘어올때 이미 숫자인지 아닌지 확인이 되나..?
@@ -70,45 +64,6 @@ public class ProductSaveServiceImpl implements ProductSaveService {
 			}
 			
 		}
-		
-		// 2_4) 파일이 있는지 확인
-//		if(!upfile.getOriginalFilename().isBlank()) {
-//			
-//			// 2_5) 파일 이름 변경 -> 임의로 규칙 만들었습니다.
-//			StringBuilder sb = new StringBuilder();
-//			sb.append("Orange_");
-//			String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-//			sb.append(currentTime);
-//			sb.append("_");
-//			int num = (int)(Math.random() * 900) + 100;
-//			sb.append(num);
-//			String ext = upfile.getOriginalFilename().substring(upfile.getOriginalFilename().lastIndexOf("."));
-//			sb.append(ext);
-//			
-//			ServletContext application = session.getServletContext();
-//			String savePath = application.getRealPath("resources/files/");
-//			// 임의로 파일 하나 넣어둠
-//			
-//			try {
-//				upfile.transferTo(new File(savePath + sb.toString()));
-//			} catch(Exception e) {
-//				e.printStackTrace();
-//			}
-//			
-//			// 업로드 성공했다면
-//			product.setFileOriginName(upfile.getOriginalFilename());
-//			
-//			// full 경로
-//			product.setChangeName("/spring/resources/files/" + sb.toString());
-//			// context root 바뀌면 /spring 여기도 바뀌어야함 -> 이거 어떻게 얻더라...? -> session에 참조해서 getServletContext();
-//			// ServletContext application = session.getServletContext();
-//			// String savePath = application.getRealPath("/resources/files/");
-//			
-//		}
-		// 책임분리함 -> FileInfo는 빈등록 안해두 되나..? 그냥 VO라서?
-//		FileInfo fileInfo = fileUpload.saveFile(upfile, session);
-		
-		// -------------------------------------------------- 사실 1부터 2까지는 전부 책임분리해야함
 		
 		// 3. insert 하러 Mapper에 보냄
 		int result = productSaveMapper.save(product);
