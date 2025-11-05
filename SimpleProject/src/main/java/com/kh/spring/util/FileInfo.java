25723f0a7237e2124959043c2c69024b2e236391
s2selyn <s2selyn@gmail.com>
2025-11-05 19:03:34 +0900

    setup file upload class

---
+++ SimpleProject/src/main/java/com/kh/spring/util/FileInfo.java
@@ -0,0 +1,19 @@
+package com.kh.spring.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FileInfo {
+	
+	private String fileOriginName;
+	private String changeName;
+
+}
---
+++ SimpleProject/src/main/java/com/kh/spring/util/FileUpload.java
@@ -0,0 +1,22 @@
+package com.kh.spring.util;
+
+// @Component 나중에 빈등록하기
+public class FileUpload {
+	
+	// 이 메소드를 호출하면 파일을 업로드하고 파일 정보를 반환?
+	public FileInfo getFileInfo() {
+		
+		// 매개변수로 원본파일 정보를 받고
+		
+		// 파일 이름 바꿔주고
+		
+		// 파일 업로드 메소드 호출해서 업로드하고
+		
+		
+		return new FileInfo();
+		
+	}
+	
+	// 그럼 파일 업로드는 따로 메소드를 만들어서 책임분리해야함?
+
+}
